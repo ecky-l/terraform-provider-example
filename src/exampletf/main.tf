0dc7cff73f44549323a116c9fe27d3abe2b2ce56
Eckhard Lehmann <eckhard.lehmann@otto.de>
2025-10-01 20:00:00 +0200

    added exampletf terraform code

---
+++ src/exampletf/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    example = {
+      source  = "example.com/example"
+      version = "0.0.1"
+  }
+}
+
+provider "example" {
+  host = "http://localhost:8080"
+}
+
+resource "example_shop_article" "example" {
+  name        = "Princess Rosalea"
+  description = "Child Shampoo & Conditioner"
+}
